bd8fedd0de72108622dcaf10d44c54fac5303908
defredemi <demi.defre@student.hogent.be>
2024-11-06 19:24:36 +0100

    x

--- sample-app.sh
+++ sample-app.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
+# Maak benodigde mappen aan
 mkdir -p tempdir
 mkdir -p tempdir/templates
 mkdir -p tempdir/static
 
+# Kopieer bestanden naar de tempdir
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
+# Maak de Dockerfile
 cat > tempdir/Dockerfile << _EOF_
 FROM python
 RUN pip install flask
@@ -19,14 +22,18 @@ EXPOSE 5050
 CMD python /home/myapp/sample_app.py
 _EOF_
 
+# Navigeer naar tempdir
 cd tempdir || exit
-docker build -t sampleapp .
-docker run -t -d -p 5050:5050 --name samplerunning sampleapp
-docker ps -a 
 
-# Verwijder de container als deze al bestaat
+# Verwijder bestaande container indien aanwezig
 docker rm -f samplerunning || true
+
+# Bouw het Docker-image
+docker build -t sampleapp .
 
 # Start de nieuwe container
 echo "Starting a new container"
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+
+# Toon alle containers
+docker ps -a
